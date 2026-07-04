8be460202dab264a325f93772aa8265bf210d23c
k0mmaa <dmitri18@yandex.ru>
2026-07-04 20:50:40 +0300

    18 спринт - отправка доработок

--- app/src/main/java/com/example/playlistmaker/media/ui/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/MediaActivity.kt
@@ -7,36 +7,33 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import com.example.playlistmaker.R
-import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.tabs.TabLayout
+import com.example.playlistmaker.databinding.ActivityMediaBinding
 import com.google.android.material.tabs.TabLayoutMediator
-import androidx.viewpager2.widget.ViewPager2
 
 class MediaActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMediaBinding
     private lateinit var tabLayoutMediator: TabLayoutMediator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMediaBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_media)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.parent_layout_media)) { v, insets ->
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.parentLayoutMedia) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.updatePadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
-        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
-        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
-
-        toolbar.setNavigationOnClickListener {
+        binding.toolbar.setNavigationOnClickListener {
             finish()
         }
 
-        viewPager.adapter = MediaViewPagerAdapter(supportFragmentManager, lifecycle)
+        binding.viewPager.adapter = MediaViewPagerAdapter(supportFragmentManager, lifecycle)
 
-        tabLayoutMediator = TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+        tabLayoutMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             when (position) {
                 0 -> tab.text = getString(R.string.favorite_tracks)
                 1 -> tab.text = getString(R.string.playlists)
--- app/src/main/java/com/example/playlistmaker/media/ui/PlaylistsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/PlaylistsFragment.kt
@@ -5,18 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.FragmentPlaylistsBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PlaylistsFragment : Fragment() {
 
     private val viewModel by viewModel<PlaylistsViewModel>()
 
+    private var _binding: FragmentPlaylistsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_playlists, container, false)
+    ): View {
+        _binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
